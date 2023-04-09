6a4a05ee28ba1e543b201acce34436b0cb867f98
Ilmar7786 <Ilya.martynov63@gmail.com>
2023-04-09 10:51:57 +0300

    add front init

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,6 @@
+import {type FC} from "react";
+import {HomePage} from "../pages/Home";
+
+export const App: FC = () => {
+    return <HomePage/>
+}
---
+++ frontend/src/pages/Home/Home.page.tsx
@@ -0,0 +1,9 @@
+import {FC} from "react";
+
+export const HomePage: FC = () => {
+    return (
+        <main>
+            <h1>Личный кабинет</h1>
+        </main>
+    )
+}
